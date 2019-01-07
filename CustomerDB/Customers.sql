e075b02fa55768df43a56e0a1a54b62c62ced94c
Robert Robinson <robinson6113@student.cptc.edu>
2019-01-07 14:19:12 -0800

    Created a Customer table and added some test data

--- CustomerDB/Customers.sql
+++ CustomerDB/Customers.sql
@@ -1,1 +1,13 @@
 ﻿
+CREATE TABLE Customer
+(
+	ID		int				PRIMARY KEY		IDENTITY
+	,Name	varchar(30)		NOT NULL
+)
+
+INSERT INTO Customer 
+VALUES ('Rob')
+	, ('Jen')
+	, ('Dave')
+	, ('Rich')
+	, ('Shelly')
