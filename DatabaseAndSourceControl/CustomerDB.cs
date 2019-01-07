98a4610090d7b3ad8efdfc77e490caa1d06fb175
Robert Robinson <robinson6113@student.cptc.edu>
2019-01-07 14:56:29 -0800

    Created CustomerDB class, started to code CRUD functionality

---
+++ DatabaseAndSourceControl/CustomerDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAndSourceControl
+{
+    static class CustomerDB
+    {
+        public static List<string> GetAllNames()
+        {
+            string connection = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=CustomerDB;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                //TODO: Finish database code
+                con.Close();
+            }
+
+            throw new NotImplementedException();
+
+            //SqlConnection con = new SqlConnection(connection);
+
+            //try
+            //{
+            //    con.Open();
+            //}
+            //finally
+            //{
+            //    con.Dispose();
+            //}
+        }
+    }
+}
